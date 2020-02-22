4088f3755b3e6705d0c3386122722e65fc9a5b06
yeojin_choi <yeojin_choi@tmax.co.kr>
2020-02-22 18:53:10 +0900

    [feat] Book 10.3.1~2 입력 상태 관리, 일정 리스트  바인딩

--- todo-list/src/components/App.js
+++ todo-list/src/components/App.js
@@ -4,11 +4,28 @@ import TodoInput from './TodoInput/TodoInput';
 import TodoList from './TodoList/TodoList';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: '',
+            todos: [{ id: 0, text: '리액트 공부하기', done: true }, { id: 1, text: '컴포넌트 스타일링 해보기', done: false }]
+        };
+    }
+
+    handleChange = e => {
+        const { value } = e.target;
+        this.setState({
+            input: value
+        });
+    };
+
     render() {
+        const { input, todos } = this.state;
+        const { handleChange } = this;
         return (
             <PageTemplate>
-                <TodoInput />
-                <TodoList />
+                <TodoInput onChange={handleChange} value={input} />
+                <TodoList todos={todos} />
             </PageTemplate>
         );
     }
