e8686723232071e49086e8d19b6fdbdf72a79fc1
yeojin_choi <yeojin_choi@tmax.co.kr>
2020-02-22 19:42:27 +0900

    [feat] Book 10.1.4 Counter Component 생성

--- redux-counter/src/containers/App.js
+++ redux-counter/src/containers/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import Counter from '../components/Counter';
 
 class App extends Component {
     render() {
-        return <div>Counter</div>;
+        return (
+            <div>
+                <Counter />
+            </div>
+        );
     }
 }
 
