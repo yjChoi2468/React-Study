6a082ef2f48790383cca9d8db4891e2302da110c
yeojin_choi <yeojin_choi@tmax.co.kr>
2020-02-26 21:11:09 +0900

    [feat] Book 개정판 7.3 componentDidCatch 추가

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,46 +1,48 @@
 import React, { Component } from 'react';
 import LifeCycleSample from './LifeCycleSample';
-import ScrollBox from './ScrollBox';
+import ErrorBoundary from './ErrorBoundary';
 
-/**
- * ScrollBox 예제 에러나는거 원인 및 해결법: createRef() 로 ref 생성 시, .current로 조회
- */
-function App() {
-    let scrollBox = React.createRef();
-
-    return (
-        <div>
-            <ScrollBox ref={scrollBox}></ScrollBox>
-            <button onClick={() => scrollBox.current.scrollToBottom()}>맨 밑으로</button>
-        </div>
-    );
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-// function getRandomColor() {
-//     return '#' + Math.floor(Math.random() * 16777215).toString(16);
-// }
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: '#000000'
+        };
+    }
+    handleClick = () => {
+        this.setState({
+            color: getRandomColor()
+        });
+    };
 
-// class App extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             color: '#000000'
-//         };
-//     }
-//     handleClick = () => {
-//         this.setState({
-//             color: getRandomColor()
-//         });
-//     };
-
-//     render() {
-//         return (
-//             <div>
-//                 <button onClick={this.handleClick}>Random Color</button>
-//                 <LifeCycleSample color={this.state.color}></LifeCycleSample>
-//             </div>
-//         );
-//     }
-// }
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClick}>Random Color</button>
+                <ErrorBoundary>
+                    <LifeCycleSample color={this.state.color}></LifeCycleSample>
+                </ErrorBoundary>
+            </div>
+        );
+    }
+}
 
 export default App;
+
+/**
+ * ScrollBox 예제 에러나는거 원인 및 해결법 확인 필요
+ */
+// function App() {
+//     let scrollBox = React.createRef();
+
+//     return (
+//         <div>
+//             <ScrollBox ref={scrollBox}></ScrollBox>
+//             <button onClick={() => scrollBox.scrollToBottom()}>맨 밑으로</button>
+//         </div>
+//     );
+// }
