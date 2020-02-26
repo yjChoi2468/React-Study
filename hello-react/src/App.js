e6fab97e21cf23156e0b1f8745ef7bf18ca5fe99
yeojin_choi <yeojin_choi@tmax.co.kr>
2020-02-26 17:06:23 +0900

    [bugfix] Book 5.3 createRef() 로 ref 생성 시, .current로 조회

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,45 +1,46 @@
 import React, { Component } from 'react';
 import LifeCycleSample from './LifeCycleSample';
-
-function getRandomColor() {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            color: '#000000'
-        };
-    }
-    handleClick = () => {
-        this.setState({
-            color: getRandomColor()
-        });
-    };
-
-    render() {
-        return (
-            <div>
-                <button onClick={this.handleClick}>Random Color</button>
-                <LifeCycleSample color={this.state.color}></LifeCycleSample>
-            </div>
-        );
-    }
-}
-
-export default App;
+import ScrollBox from './ScrollBox';
 
 /**
- * ScrollBox 예제 에러나는거 원인 및 해결법 확인 필요
+ * ScrollBox 예제 에러나는거 원인 및 해결법: createRef() 로 ref 생성 시, .current로 조회
  */
-// function App() {
-//     let scrollBox = React.createRef();
+function App() {
+    let scrollBox = React.createRef();
 
-//     return (
-//         <div>
-//             <ScrollBox ref={scrollBox}></ScrollBox>
-//             <button onClick={() => scrollBox.scrollToBottom()}>맨 밑으로</button>
-//         </div>
-//     );
+    return (
+        <div>
+            <ScrollBox ref={scrollBox}></ScrollBox>
+            <button onClick={() => scrollBox.current.scrollToBottom()}>맨 밑으로</button>
+        </div>
+    );
+}
+
+// function getRandomColor() {
+//     return '#' + Math.floor(Math.random() * 16777215).toString(16);
 // }
+
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             color: '#000000'
+//         };
+//     }
+//     handleClick = () => {
+//         this.setState({
+//             color: getRandomColor()
+//         });
+//     };
+
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.handleClick}>Random Color</button>
+//                 <LifeCycleSample color={this.state.color}></LifeCycleSample>
+//             </div>
+//         );
+//     }
+// }
+
+export default App;
