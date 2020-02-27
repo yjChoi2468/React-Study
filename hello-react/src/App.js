853a1e1ecf4e6f9c937aa2cadbafddd38e33d319
yeojin_choi <yeojin_choi@tmax.co.kr>
2020-02-27 11:04:09 +0900

    [feat] 개정판 8.1 useState 실습

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,48 +1,83 @@
-import React, { Component } from 'react';
-import LifeCycleSample from './LifeCycleSample';
-import ErrorBoundary from './ErrorBoundary';
+// import React, { Component } from 'react';
+// import LifeCycleSample from './LifeCycleSample';
+// import ErrorBoundary from './ErrorBoundary';
 
-function getRandomColor() {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+// function getRandomColor() {
+//     return '#' + Math.floor(Math.random() * 16777215).toString(16);
+// }
+
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             color: '#000000'
+//         };
+//     }
+//     handleClick = () => {
+//         this.setState({
+//             color: getRandomColor()
+//         });
+//     };
+
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.handleClick}>Random Color</button>
+//                 <ErrorBoundary>
+//                     <LifeCycleSample color={this.state.color}></LifeCycleSample>
+//                 </ErrorBoundary>
+//             </div>
+//         );
+//     }
+// }
+
+
+
+// /**
+//  * ScrollBox 예제 에러나는거 원인 및 해결법 확인 필요
+//  */
+// // function App() {
+// //     let scrollBox = React.createRef();
+
+// //     return (
+// //         <div>
+// //             <ScrollBox ref={scrollBox}></ScrollBox>
+// //             <button onClick={() => scrollBox.scrollToBottom()}>맨 밑으로</button>
+// //         </div>
+// //     );
+// // }
+import React, { useRef, createRef, useState } from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  const [renderIndex, setRenderIndex] = useState(1);
+  const refFromUseRef = useRef();
+  const refFromCreateRef = createRef();
+  if (!refFromUseRef.current) {
+    refFromUseRef.current = renderIndex;
+  }
+  if (!refFromCreateRef.current) {
+    refFromCreateRef.current = renderIndex;
+  }
+  return (
+    <div className="App">
+      Current render index: {renderIndex}
+      <br />
+      First render index remembered within refFromUseRef.current:
+      {refFromUseRef.current}
+      <br />
+      First render index unsuccessfully remembered within
+      refFromCreateRef.current:
+      {refFromCreateRef.current}
+      <br />
+      <button onClick={() => setRenderIndex(prev => prev + 1)}>
+        Cause re-render
+      </button>
+    </div>
+  );
 }
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            color: '#000000'
-        };
-    }
-    handleClick = () => {
-        this.setState({
-            color: getRandomColor()
-        });
-    };
-
-    render() {
-        return (
-            <div>
-                <button onClick={this.handleClick}>Random Color</button>
-                <ErrorBoundary>
-                    <LifeCycleSample color={this.state.color}></LifeCycleSample>
-                </ErrorBoundary>
-            </div>
-        );
-    }
-}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;
-
-/**
- * ScrollBox 예제 에러나는거 원인 및 해결법 확인 필요
- */
-// function App() {
-//     let scrollBox = React.createRef();
-
-//     return (
-//         <div>
-//             <ScrollBox ref={scrollBox}></ScrollBox>
-//             <button onClick={() => scrollBox.scrollToBottom()}>맨 밑으로</button>
-//         </div>
-//     );
-// }
---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Info from './Info';
+
+function App() {
+    return <Info />;
+}
+
+export default App;
