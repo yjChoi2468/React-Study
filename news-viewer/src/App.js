3bdafe371cd25a3f01a4bc3b1afc4d1b38c901f5
yeojin_choi <yeojin_choi@tmax.co.kr>
2020-02-29 20:44:25 +0900

    [feat] 개정판 10.6 뉴스 카테고리 실습 완료

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
 function App() {
-  return <NewsList />;
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category => setCategory(category), []);
+  return (
+    <>
+      <Categories onSelect={onSelect} category={category} />
+      <NewsList category={category} />
+    </>
+  );
 }
 
 export default App;
