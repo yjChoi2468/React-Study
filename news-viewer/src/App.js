976a2a476f57700d740160adee20324a1aaa0a09
yeojin_choi <yeojin_choi@tmax.co.kr>
2020-02-29 19:59:59 +0900

    [feat] 개정판 14.2 axios 실습 완료

---
+++ news-viewer/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState(null);
+  // const onClick = () => {
+  //   axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+  //     setData(response.data);
+  //   });
+  // };
+  const onClick = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly />}
+    </div>
+  );
+}
+
+export default App;
