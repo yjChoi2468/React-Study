49353884f08b7213e737f7385e71b01fe01863cd
yeojin_choi <yeojin_choi@tmax.co.kr>
2020-02-29 15:42:44 +0900

    [feat] 개정판 10 todo app 만들기 준비

---
+++ new-todo-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Todo App을 만들자!
+    </div>
+  );
+}
+
+export default App;
