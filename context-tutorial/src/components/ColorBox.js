470b1b57d4cf15e812de206e2ce4d62e047afc0f
yeojin_choi <yeojin_choi@tmax.co.kr>
2020-03-02 12:46:47 +0900

    [feat] 개정판 15.3 동적 contextAPI 실습 완료

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import ColorContext from '../contexts/color';
+import { ColorConsumer } from '../contexts/color';
 
 const ColorBox = () => {
-    return <ColorContext.Consumer>{value => <div style={{ width: '64px', height: '64px', background: value.color }} />}</ColorContext.Consumer>;
+    return (
+        <ColorConsumer>
+            {({ state }) => (
+                <>
+                    <div style={{ width: '64px', height: '64px', background: state.color }} />
+                    <div style={{ width: '32px', height: '32px', background: state.subcolor }} />
+                </>
+            )}
+        </ColorConsumer>
+    );
 };
 
 export default ColorBox;
